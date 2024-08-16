d2ae4ab539052ad08a44da9d7211f7f10c17b49c
R Abdul Rahaman <abdulqa8500@gmail.com>
2024-08-16 11:10:18 +0530

    File for 10th aug 2024

---
+++ src/Lab003.py
@@ -0,0 +1,7 @@
+a, b, c, s = 45, 54, 67, 'test'
+print(a, b, c, s, sep="_")
+print(a, b, c, s, sep=" ")
+
+
+b = True
+f = False
--- src/ex_08082024/Lab001.py
+++ src/ex_08082024/Lab001.py
@@ -1,4 +1,3 @@
 # This is comment - which does not run.
 print("First pyton program")
 print(2+2+2)
-print(2-2-2)
